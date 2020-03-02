78032714a21514c1d3ffb7599020be64a4adc26e
lively-bundle-0725 <tbandachit@gmail.com>
2020-03-02 08:00:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,7 @@
 def square_array(array)
-i = 0
-
-while i < array.length do
-  array[i] = array[i] * array[i]
-  i++
+counter = 0
+while counter < array.length do
+  array[counter] = Math.sqrt(array[counter])
+  counter++
 end
-return arraylearn
 end
